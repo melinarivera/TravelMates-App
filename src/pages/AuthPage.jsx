c310c635eee6cd9459619af4da8db955579e41ef
Melina <melina.rivera@gmail.com>
2026-04-26 01:37:12 +0200

    style: increase TravelMates logo size in AuthPage

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -52,7 +52,7 @@ export default function AuthPage() {
         {/* Lado Branding (Landing) */}
         <div className="auth-brand">
           <div className="auth-logo-group">
-            <img src="/logo.svg" alt="TravelMates Logo" style={{ width: '48px', height: '48px', objectFit: 'contain' }} />
+            <img src="/logo.svg" alt="TravelMates Logo" style={{ width: '90px', height: '90px', objectFit: 'contain' }} />
             <h1 className="auth-brand-title">TravelMates</h1>
           </div>
           
